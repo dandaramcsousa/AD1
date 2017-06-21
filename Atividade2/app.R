409f77a064aed791579968f87454d18087861d7c
Dandara Sousa <dandara.sousa@ccc.ufcg.edu.br>
2017-06-20 21:24:06 -0300

    checkpoin 3.1 quase finalizado

--- Atividade2/app.R
+++ Atividade2/app.R
@@ -13,23 +13,29 @@ library(dplyr)
 library(tidyr)
 library(readr)
 library(plotly)
-
 library(knitr)
+
+series = read_csv("https://raw.githubusercontent.com/nazareno/imdb-series/master/data/series_from_imdb.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Atividade 2 - Checkpoint 2"),
+   titlePanel("Desempenho de uma série ao longo das temporadas"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         selectInput("select", label = h3("Séries"), choices = sort(unique(series$series_name)))
+         h2("Séries IMDB"),
+         p("Aqui vamos observar como se comporta uma série ao longo das temporadas. Abaixo escolha a série
+           de sua preferência:"),
+         selectInput("select", label = ("Nome da série"), choices = sort(unique(series$series_name))),
+         p("Para interagir com o gráfico basta clicar no número da temporada na direita. Selecionando as temporadas que você deseja ver. Se não houver a opção é porque a série só possui uma temporada."),
+         p("É possível observar o quanto a série cresce ou decresce de acordo com as temporadas. Uma série com várias temporadas ainda alcançam notas altas?")
       ),
-      
       # Show a plot of the generated distribution
       mainPanel(
+         h1("Nota do usuário para cada episódio"),
          plotlyOutput("distPlot")
       )
    )
@@ -37,13 +43,13 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  cresc = series %>%
-    group_by(series_name,season) %>%
-    summarise(Mediana = median(UserRating))
+ names(series)[names(series) == "season_ep"]<- "Episódio"
+  names(series)[names(series) == "UserRating"]<- "Nota"
   
    output$distPlot <- renderPlotly({
-     plot_ly(data = cresc %>% filter(series_name %in% c(input$select)),x = ~season ,y = ~Mediana) 
+     plot_ly(data = series %>% filter(series_name %in% c(input$select)),x = ~Episódio ,y = ~Nota) %>%
+       add_lines(hoverinfo = 'text', text=~paste('Episódio: ', Episódio, '<br>Temporada: ', season, '<br> Nome do episódio: ', Episode, '<br> Nota: ', Nota), color=~as.character(season))
+     
    })
 }
 
